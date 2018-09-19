ad51d0030db652e68e64dbb346198b2860be9fc5
Meghan Campbell <mlc03e@gmail.com>
2018-09-19 18:01:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var greeting = "Hello, everybody!";
-
 var specialGuest = "Neil deGrasse Tyson";
+var topic = "space"
 
-function greetSpecialGuest {
-  return `Hello, + ${specialGuest} + !`;
+function greetSpecialGuest() {
+  return "Hello, " + [specialGuest] + "!";
 }
 
-var topic = "space";
 
 var conversation = `'Let\'s talk about' + ${topic} + '.'`;
